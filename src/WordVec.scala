5c860470cf9ccc90ba38fae76e0e17096564117e
Jeevan Shankar <jshankar@blake.cs.umass.edu>
2014-03-19 01:10:15 -0400

    added codes, scipts

---
+++ src/WordVec.scala
@@ -0,0 +1,12 @@
+object WordVec {
+    def main(args : Array[String]) {
+         val opts = new EmbeddingOpts
+         opts.parse(args)
+         val skipgram = if (opts.cbow.value == 1) new CBOWNegSamplingEmbeddingModel(opts) else new SkipGramNegSamplingEmbeddingModel(opts)
+         skipgram.buildVocab()
+         val st = System.currentTimeMillis()
+         skipgram.learnEmbeddings
+         val en = System.currentTimeMillis() - st 
+         println("time taken : " + en/1000.0)
+    }
+ }
