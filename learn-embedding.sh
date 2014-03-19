a66a7e2e3e4911c19bbe94073e74ac98346f9f54
Jeevan Shankar <jshankar@blake.cs.umass.edu>
2014-03-19 17:13:03 -0400

    added script to just learn the embedding

---
+++ learn-embedding.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+## scipt for computing the vectors  and checking the accuracy with google's questions-words analogy task
+if [ ! -e text8 ]; then
+    wget https://dl.dropboxusercontent.com/u/39534006/text8.zip -O text8.gz
+    gzip -d text8.gz -f
+fi
+pwd=$PWD
+home=$HOME
+factorielib="${home}/.m2/repository/cc/factorie/factorie/1.0-SNAPSHOT/factorie-1.0-SNAPSHOT.jar"
+scalalib="${home}/.m2/repository/org/scala-lang/scala-library/2.10.2/scala-library-2.10.2.jar"
+wordvec="cn.jar"
+gcc compute-accuracy.c -o compute-accuracy -lm ## google's word2vec code
+mvn compile; mvn compile;
+if [ $? -eq 0 ]; then
+ cd target/classes
+ jar cf ${wordvec} .
+ mv ${wordvec} ../../${wordvec}
+ cd ../..
+ wordvecjar=$pwd/$wordvec
+ java -Xmx10g -cp "${wordvecjar}:${factorielib}:${scalalib}" WordVec --cbow=0 --train text8 --output vectors.txt --size=200 --window=5 --min-count=5 --threads=12 --save-vocab=text8.vocab 
+fi
+
