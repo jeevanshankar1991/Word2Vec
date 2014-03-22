0e77780bbbf55683f164c7cfd26c5386a9fda161
Jeevan Shankar <jeevanshankar1991@gmail.com>
2014-03-22 19:30:46 -0400

    Update demo-word-accuracy.sh

--- demo-word-accuracy.sh
+++ demo-word-accuracy.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 ## scipt for computing the vectors  and checking the accuracy with google's questions-words analogy task
 if [ ! -e text8 ]; then
-    wget https://dl.dropboxusercontent.com/u/39534006/text8.zip -O text8.gz
-    gzip -d text8.gz -f
+    #wget https://dl.dropboxusercontent.com/u/39534006/text8.zip -O text8.gz
+    curl -O https://dl.dropboxusercontent.com/u/39534006/text8.zip
+    unzip text8.zip
 fi
 pwd=$PWD
 home=$HOME
 factorielib="${home}/.m2/repository/cc/factorie/factorie/1.0-SNAPSHOT/factorie-1.0-SNAPSHOT.jar"
 scalalib="${home}/.m2/repository/org/scala-lang/scala-library/2.10.2/scala-library-2.10.2.jar"
-wordvec="cn.jar"
+wordvec="wordvec.jar"
 gcc compute-accuracy.c -o compute-accuracy -lm ## google's word2vec code
 mvn compile; mvn compile;
 if [ $? -eq 0 ]; then
