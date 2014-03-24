b7a2e0e67051b1328061ae500f08bbabd3793e08
Jeevan Shankar <jshankar@blake.cs.umass.edu>
2014-03-24 18:42:47 -0400

    updated all codes

--- src/WordVec.scala
+++ src/WordVec.scala
@@ -1,17 +1,17 @@
+// package cc.factorie.app.nlp.embeddings
 object WordVec {
-    def main(args : Array[String]) {
-         val opts = new EmbeddingOpts
-         opts.parse(args)
-         val skipgram =  if (opts.cbow.value == 1) new FastCBOWNegSamplingEmbeddingModel(opts) else new FastSkipGramNegSamplingEmbeddingModel(opts)
-         val st1 = System.currentTimeMillis()
-         skipgram.buildVocab()
-         val st = System.currentTimeMillis()
-         println("time taken : " + (st-st1)/1000.0)
-         skipgram.learnEmbeddings
-         val en = System.currentTimeMillis() - st  
-         //Distance.nearestNeighbours("/home/jeevan/word2vec-read-only/vectors_google.bin")
-         println("time taken : " + en/1000.0)
-         skipgram.store
-         
-    }
- }
+  def main(args: Array[String]) {
+    val opts = new EmbeddingOpts
+    opts.parse(args)
+    val wordEmbedding = if (opts.cbow.value == 1) new CBOWNegSamplingEmbeddingModel(opts) else new SkipGramNegSamplingEmbeddingModel(opts)
+    val st1 = System.currentTimeMillis()
+    wordEmbedding.buildVocab()
+    val st = System.currentTimeMillis()
+    println("time taken to build vocab : " + (st - st1) / 1000.0)
+    wordEmbedding.learnEmbeddings()
+    val en = System.currentTimeMillis() - st
+    println("time taken to learn embedding : " + en / 1000.0)
+    wordEmbedding.store()
+
+  }
+}
